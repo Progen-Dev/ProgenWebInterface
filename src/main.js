6e1baf40dcd276ea241fd465bdeca14bc7e0e382
ShowMeYourSkil <davidfranzen@t-online.de>
2020-07-23 11:00:26 +0200

    First steps. Installing new plugins.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueResource from 'vue-resource'
 
+Vue.use(Vue.axios, axios, VueResource)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default Vuex.Store({
+    state: {
+        token: '',
+        adress: 'https://pwi.progen-bot.de/api'
+    },
+    actions: {
+        
+    }
+})
